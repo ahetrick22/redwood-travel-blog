c67e6f45cd40709f3a275acbf2dcd569f9c06b01
ahetrick22 <ahetrick22@gmail.com>
2021-03-14 15:34:33 -0700

    set up redwood blog tutorial

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import BlogLayout from "src/layouts/BlogLayout/BlogLayout";
+import BlogPostsCell from 'src/components/BlogPostsCell'
+
+const HomePage = () => {
+  return (
+    <BlogLayout>
+      <BlogPostsCell />
+    </BlogLayout>  )
+}
+
+export default HomePage
